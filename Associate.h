a86ecf3055fce9022acf14579c0a0c8f2869428b
Pedro Costa <up201605339@fe.up.pt>
2017-11-13 12:12:31 +0000

    Added more comments to Associate

--- Associate.h
+++ Associate.h
@@ -48,11 +48,16 @@ public:
 
 	//Get Methods
 
-	/**
+
+	/*
+
 	 * @brief Returns the name of the sub-area where this associate works
 	 *
-	 */
+
 	std::string getWorkName() const;
+
+	*/
+
 
 	/**
 	 * @brief Returns the uniqueID of the Associate
@@ -92,12 +97,14 @@ public:
 	 */
 	void setInterestAreas(std::vector<Area *> interest);
 
-	/**
+	/*
 	 * @briefs Sets/Changes the sub-area/working area of an associate
 	 *
 	 * @param sub - SubArea pointer
-	 */
+
 	void setWork(SubArea* sub);
+
+	*/
 
 	/**
 	 * @briefs Sets/Changes the string representing the current status of an associate
@@ -116,31 +123,76 @@ public:
 	void setPaidYears(std::vector<int> paid);
 
 	/**
-	 * @briefs Sets/Changes the
+	 * @briefs Sets/Changes the amount of money the associate has
 	 *
-	 * @param annualPay - int value
+	 * @param money - the new amount of money (float)
 	 */
-	void setPersonallWallet(int money);
+	void setPersonallWallet(float money);
 
 	//General Methods
-	void addToWallet(int more_money);
-	void payFromWallet(int less_money);
+
+
+	/**
+	 * @briefs Adds a certain amount to the associate wallet
+	 *
+	 * @param more_money - the amount to be added
+	 */
+	void addToWallet(float more_money);
+
+
+
+	/**
+	 * @briefs Subtracts a certain amount from the associate wallet
+	 *
+	 * @param less_money - the amount to be subtracted
+	 */
+	void payFromWallet(float less_money);
+
+	/**
+	 * @briefs Adds a year to the vector containing all the paid years
+	 *
+	 * Applicable after doing a yearly payment
+	 *
+	 * @param year - the year paid
+	 */
 	void addPaidYear(int year);
+
+
+	/**
+	 * @briefs Effectuates the payment of a year
+	 *
+	 * Checks if the associate has enough money to do such payment
+	 * and if the associate has all the years before paid up to date.
+	 * If it has no money, throws the correspondent exception (NotEnoughMoney).
+	 * If his payments are not up to date, throws the exception NotUpToDate.
+	 *
+	 * @param year - the year to be paid
+	 */
 	void payYear(int year);
+
+
+	/**
+	 * @briefs Update the string representing the status of the associate
+	 *
+	 * Checks with the static variable of the Association Current Year to see
+	 * if the associate has the payments up to date (with the 5 years delay)
+	 * and updates the string to its correspondent value.
+	 *
+	 */
 	void updateStatus();
 
 
 
 
 private:
-	Association * association;
-	std::string name;
-	float personalWallet;
-	std::string institution;
-	std::vector<Area *> interestAreas;
+	Association * association; ///< The connection between the associate and the association
+	std::string name;  ///< The associate name
+ 	float personalWallet;  ///< The amount of money the associate has
+	std::string institution;  ///< The institution to which the associate is connected
+	std::vector<Area *> interestAreas;  ///< A vector containing pointers to all the Associate interest areas
 	std::string status;  ///< contributor, subscriber, normal, the possible status
-	std::vector<int> paidYears;
-	SubArea * workingArea;
+	std::vector<int> paidYears;  ///< A vector of integers containing all the year the Associate has paid
+	//SubArea * workingArea;
 	int uniqueID;   ///< The unique Identifier
 	static int id_provider;   ///< Will ensure that every instance of this class gets a unique identifier
 };
