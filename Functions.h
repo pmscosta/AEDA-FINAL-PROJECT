d9f551fcc66dbf9e0854df1945b9a348180bf744
highpedro <up201605339@fe.up.pt>
2017-11-16 23:00:56 +0000

    Menu dos associados

--- Functions.h
+++ Functions.h
@@ -8,20 +8,7 @@
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
-#include "Associate.h"
-#include "Area.h"
-#include "Association.h"
-#include "SubArea.h"
-#include "Event.h"
-#include "SummerSchool.h"
-#include "Conference.h"
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <unistd.h>
 
 void initialize();
 
